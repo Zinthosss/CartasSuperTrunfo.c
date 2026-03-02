3a03c8b6e0e338d9631c5f180b5e389113bb0b1c
Zinthosss <tecnicozinthos@gmail.com>
2026-03-02 19:50:17 +0000

    Adicionei calculo de densidade e pib per capita. O calculo do pib n ta 100% mas funciona.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,8 @@ int main(){
     char cidade1[50], cidade2[50];
     float area1, area2;
     float pib1, pib2;
+    float densidade1, densidade2;
+    float pibpercapita1, pibpercapita2;
     int populacao1, populacao2;
     int pontosturisticos1, pontosturisticos2;
 
@@ -59,7 +61,37 @@ int main(){
     printf("Digite o número de pontos turísticos: ");
     scanf("%d", &pontosturisticos2);
 
+    // Cálculos
+    densidade1 = populacao1 / area1;
+    densidade2 = populacao2 / area2;
+
+    pibpercapita1 = pib1 / populacao1;
+    pibpercapita2 = pib2 / populacao2;
+
     printf("\nCadastro realizado com sucesso!\n");
+
+    // Resultados
+    printf("\nCARTA 1\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %3s\n", codigo1);
+    printf("Cidade: %49s\n", cidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: R$ %.2f milhões\n", pib1);
+    printf("Pontos turísticos: %d\n", pontosturisticos1);
+    printf("Densidade populacional: %.2f habitantes/km²\n", densidade1);
+    printf("PIB per capita: R$ %.2f mil\n", pibpercapita1);
+
+    printf("\nCARTA 2\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %3s\n", codigo2);
+    printf("Cidade: %49s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: R$ %.2f bilhões\n", pib2);
+    printf("Pontos turísticos: %d\n", pontosturisticos2);
+    printf("Densidade populacional: %.2f habitantes/km²\n", densidade2);
+    printf("PIB per capita: R$ %.2f mil\n", pibpercapita2);
 
     return 0;
 }
